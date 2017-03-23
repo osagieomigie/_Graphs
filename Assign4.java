4b29a2a1e835170cd70b15bf44a44e2bc713dd6b
osagieomigie <osagieomigie@gmail.com>
2017-03-23 17:37:58 -0600

    Program reads adjacency matrix from file and stores it into array

---
+++ Assign4.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Assign4{
+	
+	public static void main(String[] args) {
+		
+		try{
+			FileReader reader = new FileReader(args[0]);
+			BufferedReader breader = new BufferedReader(reader);
+
+			String line; 
+			String [] array; 
+			int [][]adj = new int [31][31];
+			int source = 0; 
+			int row =0; 
+			
+			while( (line = breader.readLine()) != null ){
+				array = line.split("\\s+");
+				for(int a =0; a <= 30; a++){
+					adj[row][a] = Integer.parseInt(array[a]);
+				}
+				row++;
+			}	
+
+			for (int i = 0; i <= 30; i++) {
+				for (int j = 0; j <= 30; j++) 
+					System.out.printf("%d ", adj[i][j]);
+			System.out.println();
+			}
+			reader.close();
+		}catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}	
+}
